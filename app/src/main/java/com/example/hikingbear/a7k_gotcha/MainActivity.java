c636aca7182c1aa2416d14439e793af21b6c4ee6
HikingBe4r <kimisa0927@gmail.com>
2016-10-12 19:28:15 +0900

    로그인기능 추가시작

--- app/src/main/java/com/example/hikingbear/a7k_gotcha/MainActivity.java
+++ app/src/main/java/com/example/hikingbear/a7k_gotcha/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.hikingbear.a7k_gotcha;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.content.SharedPreferences;
+import android.widget.CheckBox;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText idInput, passwordInput;
+    CheckBox autoLogin;
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout);
+
+        idInput = (EditText) findViewById(R.id.emailInput);
+        passwordInput = (EditText) findViewById(R.id.passwordInput);
+        autoLogin = (CheckBox) findViewById(R.id.checkBox);
     }
 }
